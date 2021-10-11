1e55005fad8aa12b3597e0f5bd5689cf3da87fcd
take566 <tmf566@gmail.com>
2021-10-11 14:32:44 +0900

    修正

--- nas_mount.bat
+++ nas_mount.bat
@@ -5,5 +5,5 @@ net use Y: /DELETE
 net use Z: /DELETE
 
 net use X: \\AS4002T-A6F7\Music /user:admin asdf1242
-net use Y: \\AS4002T-A6F7\Media /user:admin asdf1242
-net use Z: \\AS4002T-A6F7\Home /user:admin asdf1242
+net use Y: \\AS4002T-A6F7\Media 
+net use Z: \\AS4002T-A6F7\Home 
