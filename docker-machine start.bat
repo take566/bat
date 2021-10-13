9665b4c79ad5b006e7edb459f42b539be1ee535c
take566 <tmf566@gmail.com>
2021-10-13 10:32:45 +0900

    no message

--- docker-machine start.bat
+++ docker-machine start.bat
@@ -1,9 +1,28 @@
-docker-machine rm default -y
+@ECHO OFF
+
+echo %date%
+echo %time%
+ 
+set yyyy=%date:~0,4%
+set mm=%date:~5,2%
+set dd=%date:~8,2%
+ 
+set time2=%time: =0%
+ 
+set hh=%time2:~0,2%
+set mn=%time2:~3,2%
+set ss=%time2:~6,2%
+ 
+set filename=%yyyy%-%mm%%dd%-%hh%%mn%%ss%
+SET LOG=C:\work\log\docker_%filename%.txt
 
 
-docker-machine create --driver virtualbox default
-docker-machine start default
-docker-machine env default
-eval "$(docker-machine env default)"
-docker-machine ip default
-#docker images | awk '/<none/{print $3}' | xargs docker rmi
+docker-machine rm default -y >> %LOG%
+
+
+docker-machine create --virtualbox-no-vtx-check -d virtualbox default >> %LOG%
+docker-machine start default >> %LOG%
+docker-machine env default >> %LOG%
+eval "$(docker-machine env default)"  >> %LOG%
+docker-machine ip default >> %LOG%
+#docker images | awk '/<none/{print $3}' | xargs docker rmi >> %LOG%
