0e7ca383444dae0204d1109681d580fd95db4186
take566 <tmf566@gmail.com>
2021-12-22 10:18:10 +0900

    リソース改善

--- minikube start - hv.bat
+++ minikube start - hv.bat
@@ -1,7 +1,7 @@
 #cd /d C:\work\kubernetes
 minikube stop
 minikube delete
-minikube --vm-driver=hyperv --driver=docker start  --memory 16384
+minikube --vm-driver=hyperv --driver=docker --extra-config=apiserver.Authorization.Mode=RBAC start  --cpus 4 --memory 8192 
 minikube addons enable ingress
 
 minikube ip
