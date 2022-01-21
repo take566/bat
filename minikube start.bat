b096a08a11c1084d7397a6a777dc3d4d45bd7638
take566 <tmf566@gmail.com>
2022-01-21 17:23:44 +0900

    Update minikube start.bat

--- minikube start.bat
+++ minikube start.bat
@@ -1,4 +1,4 @@
 #cd /d D:
 minikube stop
 minikube delete
-minikube --vm-driver=virtualbox --driver=docker --extra-config=apiserver.Authorization.Mode=RBAC start  --cpus 4 --memory 8192 
+minikube --vm-driver=virtualbox --driver=docker start  --cpus 4 --memory 8192 
