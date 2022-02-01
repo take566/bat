3c28618a1d355c520aa6a762cde1121716ec8fc0
TMF-DESKTOP\tmf58 <tmf566@gmail.com>
2022-02-01 15:18:51 +0900

    修正

--- minikube start - hv.bat
+++ minikube start - hv.bat
@@ -1,7 +1,23 @@
-#cd /d C:\work\kubernetes
-minikube stop
-minikube delete
-minikube --vm-driver=hyperv --driver=docker --extra-config=apiserver.Authorization.Mode=RBAC start  --cpus 4 --memory 8192 
-minikube addons enable ingress
+echo %date%
+echo %time%
 
-minikube ip
+SET yyyy=%date:~0,4%
+SET mm=%date:~5,2%
+SET dd=%date:~8,2%
+
+SET time2=%time: =0%
+
+SET hh=%time2:~0,2%
+SET mn=%time2:~3,2%
+SET ss=%time2:~6,2%
+
+SET filename=%yyyy%-%mm%%dd%-%hh%%mn%%ss%
+SET LOG=C:\work\log\minikube_hv_%filename%.txt
+
+cd /d C:\work\kubernetes
+minikube stop >> %LOG%
+minikube delete >> %LOG%
+minikube --vm-driver=hyperv --driver=docker --extra-config=apiserver.Authorization.Mode=RBAC start  --cpus 4 --memory 8192 >> %LOG%
+minikube addons enable ingress >> %LOG%
+
+minikube ip >> %LOG%
--- minikube start.bat
+++ minikube start.bat
@@ -1,4 +1,21 @@
-#cd /d D:
-minikube stop
-minikube delete
-minikube --vm-driver=virtualbox --driver=docker start  --cpus 4 --memory 8192 
+echo %date%
+echo %time%
+
+SET yyyy=%date:~0,4%
+SET mm=%date:~5,2%
+SET dd=%date:~8,2%
+
+SET time2=%time: =0%
+
+SET hh=%time2:~0,2%
+SET mn=%time2:~3,2%
+SET ss=%time2:~6,2%
+
+SET filename=%yyyy%-%mm%%dd%-%hh%%mn%%ss%
+SET LOG=C:\work\log\minikube_%filename%.txt
+
+cd /d C:\work\kubernetes  >> %LOG%
+
+minikube stop >> %LOG%
+minikube delete >> %LOG%
+minikube --vm-driver=virtualbox --driver=docker start  --cpus 4 --memory 8192 >> %LOG%
