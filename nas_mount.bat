10abeb4d9b9e4d973490cebee88091570a4c9e8e
take566 <tmf566@gmail.com>
2024-05-06 10:20:26 +0900

    Update NAS mount script to include new network drive J

--- nas_mount.bat
+++ nas_mount.bat
@@ -1,9 +1,10 @@
 
-
+net use J: /DELETE /y
 net use X: /DELETE /y
 net use Y: /DELETE /y
 net use Z: /DELETE /y
 
 net use X: \\AS4002T-A6F7\Music /user:admin asdf1242
 net use Y: \\AS4002T-A6F7\Media 
-net use Z: \\AS4002T-A6F7\Home 
+net use Z: \\AS4002T-A6F7\Home
+net use J: \\AS4002T-A6F7\Docker  
