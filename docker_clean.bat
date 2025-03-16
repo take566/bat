139da302cfed13f5ac737e2cffbf4b502827bbfe
take <tmf566@gmail.com>
2025-03-16 18:22:51 +0900

    chore: Enhance docker_clean.bat and robocopy.bat with logging and directory creation features

--- docker_clean.bat
+++ docker_clean.bat
@@ -10,7 +10,11 @@ SET time2=%time: =0%
 SET hh=%time2:~0,2%
 SET mn=%time2:~3,2%
 SET ss=%time2:~6,2%
-
+REM Check if the log directory exists, create it if it doesn't
+IF NOT EXIST "%LOG_DIR%" (
+    MKDIR "%LOG_DIR%"
+    echo Created log directory: %LOG_DIR%
+)
 SET filename=%yyyy%-%mm%%dd%-%hh%%mn%%ss%
 SET LOG=C:\work\log\docker_clean_%filename%.txt
 docker system prune -y >> %LOG%
--- robocopy.bat
+++ robocopy.bat
@@ -1,1 +1,12 @@
-robocopy  /S /COPY:DAT /LOG:"D:\bk\log.log" "R:\" "D:\bk"
+@echo off
+set YYYY=%date:~0,4%
+set MM=%date:~5,2%
+set DD=%date:~8,2%
+set BACKUP_DATE=%YYYY%-%MM%-%DD%
+set BACKUP_DIR=Z:\bk\%BACKUP_DATE%
+set LOG_FILE=Z:\bk\logs\backup_%BACKUP_DATE%.log
+
+if not exist "Z:\bk\logs" mkdir "Z:\bk\logs"
+if not exist "%BACKUP_DIR%" mkdir "%BACKUP_DIR%"
+
+robocopy "c:\work" "%BACKUP_DIR%" /S /COPY:DAT /LOG:"%LOG_FILE%"
