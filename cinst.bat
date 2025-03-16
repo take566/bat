12d8aeb03149cde62bcd578021ec64f8094ace05
take <tmf566@gmail.com>
2025-03-16 18:46:38 +0900

    chore: Add logging and directory creation to cinst.bat for better tracking and organization

--- cinst.bat
+++ cinst.bat
@@ -1,3 +1,26 @@
+echo %date%
+echo %time%
+
+SET yyyy=%date:~0,4%
+SET mm=%date:~5,2%
+SET dd=%date:~8,2%
+
+SET time2=%time: =0%
+
+SET hh=%time2:~0,2%
+SET mn=%time2:~3,2%
+SET ss=%time2:~6,2%
+
+SET filename=%yyyy%-%mm%%dd%-%hh%%mn%%ss%
+
+SET LOG_DIR=C:\work\log
+SET LOG=%LOG_DIR%\cinst_%filename%.txt
+
+REM Check if the log directory exists, create it if it doesn't
+IF NOT EXIST "%LOG_DIR%" (
+    MKDIR "%LOG_DIR%"
+    echo Created log directory: %LOG_DIR%
+)
 choco install 1password8 -y
 choco install 7zip.install -y
 choco install adobereader -y
