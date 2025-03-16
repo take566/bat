89ac9d20e140aeebe0037ff77cfb3395f296eb78
take <tmf566@gmail.com>
2025-03-16 13:01:54 +0900

    chore: Update choco update.bat to create log directory if it doesn't exist

--- choco update.bat
+++ choco update.bat
@@ -1,20 +1,27 @@
 @echo off
 echo %date%
 echo %time%
- 
+
 SET yyyy=%date:~0,4%
 SET mm=%date:~5,2%
 SET dd=%date:~8,2%
- 
+
 SET time2=%time: =0%
- 
+
 SET hh=%time2:~0,2%
 SET mn=%time2:~3,2%
 SET ss=%time2:~6,2%
- 
+
 SET filename=%yyyy%-%mm%%dd%-%hh%%mn%%ss%
 
-SET LOG=C:\work\log\clist_%filename%.txt
+SET LOG_DIR=C:\work\log
+SET LOG=%LOG_DIR%\clist_%filename%.txt
+
+REM Check if the log directory exists, create it if it doesn't
+IF NOT EXIST "%LOG_DIR%" (
+    MKDIR "%LOG_DIR%"
+    echo Created log directory: %LOG_DIR%
+)
 
 choco upgrade all -y >> %LOG%
 
