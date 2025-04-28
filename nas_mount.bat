7622695e881a7d27c0c2fe86a15fab831968961c
Cline Assistant <cline@example.com>
2025-04-29 04:28:02 +0900

    chore: nas_mount.batのコードを整理し、不要なリトライロジックを削除

--- nas_mount.bat
+++ nas_mount.bat
@@ -21,100 +21,11 @@ IF NOT EXIST "%LOG_DIR%" (
 SET filename=%yyyy%-%mm%%dd%-%hh%%mn%%ss%
 SET LOG=C:\work\log\nas_mount_%filename%.txt
 
-echo マウント処理を開始します... >> "%LOG%" 2>&1
-echo 既存の接続を切断しています... >> "%LOG%" 2>&1
+REM 既存の接続を解除
+net use X: /DELETE /y
+net use Y: /DELETE /y
+net use Z: /DELETE /y
 
-net use J: /DELETE /y >> "%LOG%" 2>&1
-net use X: /DELETE /y >> "%LOG%" 2>&1
-net use Y: /DELETE /y >> "%LOG%" 2>&1
-net use Z: /DELETE /y >> "%LOG%" 2>&1
-
-echo 切断完了 >> "%LOG%" 2>&1
-echo. >> "%LOG%" 2>&1
-
-REM リトライ回数の設定
-SET MAX_RETRY=3
-SET SERVER=\\AS4002T-A6F7
-
-echo NASへの接続を開始します... >> "%LOG%" 2>&1
-
-:MOUNT_X
-SET RETRY=0
-echo X: ドライブのマウントを試みます... >> "%LOG%" 2>&1
-:RETRY_X
-net use X: %SERVER%\Music /user:admin /savecred /persistent:yes >> "%LOG%" 2>&1
-IF %ERRORLEVEL% NEQ 0 (
-    SET /A RETRY+=1
-    echo X: ドライブのマウント失敗 - リトライ %RETRY%/%MAX_RETRY% >> "%LOG%" 2>&1
-    IF %RETRY% LSS %MAX_RETRY% (
-        timeout /t 5
-        GOTO RETRY_X
-    ) ELSE (
-        echo X: ドライブのマウント失敗 - 最大リトライ回数に到達 >> "%LOG%" 2>&1
-    )
-) ELSE (
-    echo X: ドライブのマウント成功 >> "%LOG%" 2>&1
-)
-
-:MOUNT_Y
-SET RETRY=0
-echo Y: ドライブのマウントを試みます... >> "%LOG%" 2>&1
-:RETRY_Y
-net use Y: %SERVER%\Media /user:admin /savecred /persistent:yes >> "%LOG%" 2>&1
-IF %ERRORLEVEL% NEQ 0 (
-    SET /A RETRY+=1
-    echo Y: ドライブのマウント失敗 - リトライ %RETRY%/%MAX_RETRY% >> "%LOG%" 2>&1
-    IF %RETRY% LSS %MAX_RETRY% (
-        timeout /t 5
-        GOTO RETRY_Y
-    ) ELSE (
-        echo Y: ドライブのマウント失敗 - 最大リトライ回数に到達 >> "%LOG%" 2>&1
-    )
-) ELSE (
-    echo Y: ドライブのマウント成功 >> "%LOG%" 2>&1
-)
-
-:MOUNT_Z
-SET RETRY=0
-echo Z: ドライブのマウントを試みます... >> "%LOG%" 2>&1
-:RETRY_Z
-net use Z: %SERVER%\Home /user:admin /savecred /persistent:yes >> "%LOG%" 2>&1
-IF %ERRORLEVEL% NEQ 0 (
-    SET /A RETRY+=1
-    echo Z: ドライブのマウント失敗 - リトライ %RETRY%/%MAX_RETRY% >> "%LOG%" 2>&1
-    IF %RETRY% LSS %MAX_RETRY% (
-        timeout /t 5
-        GOTO RETRY_Z
-    ) ELSE (
-        echo Z: ドライブのマウント失敗 - 最大リトライ回数に到達 >> "%LOG%" 2>&1
-    )
-) ELSE (
-    echo Z: ドライブのマウント成功 >> "%LOG%" 2>&1
-)
-
-:MOUNT_J
-SET RETRY=0
-echo J: ドライブのマウントを試みます... >> "%LOG%" 2>&1
-:RETRY_J
-net use J: %SERVER%\Docker /user:admin /savecred /persistent:yes >> "%LOG%" 2>&1
-IF %ERRORLEVEL% NEQ 0 (
-    SET /A RETRY+=1
-    echo J: ドライブのマウント失敗 - リトライ %RETRY%/%MAX_RETRY% >> "%LOG%" 2>&1
-    IF %RETRY% LSS %MAX_RETRY% (
-        timeout /t 5
-        GOTO RETRY_J
-    ) ELSE (
-        echo J: ドライブのマウント失敗 - 最大リトライ回数に到達 >> "%LOG%" 2>&1
-    )
-) ELSE (
-    echo J: ドライブのマウント成功 >> "%LOG%" 2>&1
-)
-
-echo.
-echo マウント処理が完了しました
-echo ログファイル: %LOG%
-
-REM マウントされたドライブの確認
-net use >> "%LOG%" 2>&1
-
-timeout /t 5
+net use X: \\AS4002T-A6F7\Music /user:admin asdf1242
+net use Y: \\AS4002T-A6F7\Media 
+net use Z: \\AS4002T-A6F7\Home 
