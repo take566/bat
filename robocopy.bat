cc494e8973b5d37c7d2b97e215eaedfa5b834772
take <tmf566@gmail.com>
2025-08-13 05:55:27 +0900

    refactor: Update backup date format and specify source directory in robocopy script

--- robocopy.bat
+++ robocopy.bat
@@ -2,11 +2,11 @@
 set YYYY=%date:~0,4%
 set MM=%date:~5,2%
 set DD=%date:~8,2%
-set BACKUP_DATE=%YYYY%-%MM%-%DD%
+set BACKUP_DATE=%YYYY%%MM%%DD%
 set BACKUP_DIR=Z:\bk\%BACKUP_DATE%
 set LOG_FILE=Z:\bk\log\backup_%BACKUP_DATE%.log
 
 if not exist "Z:\bk\logs" mkdir "Z:\bk\logs"
 if not exist "%BACKUP_DIR%" mkdir "%BACKUP_DIR%"
 
-robocopy "." "%BACKUP_DIR%" /S /COPY:DAT /LOG:"%LOG_FILE%"
+robocopy "D:\work" "%BACKUP_DIR%" /S /COPY:DAT /LOG:"%LOG_FILE%"
